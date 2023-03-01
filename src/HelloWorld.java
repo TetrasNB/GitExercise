eb5645953a57de5dd6d0f1f09ffe68f484447dca
marstonad20 <marstonad20@gcc.edu>
2023-03-01 10:38:25 -0500

    Changed to prompt user for their first name and then greets the user.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        Scanner scnr = new Scanner(System.in);
+        System.out.print("Please input your first name: ");
+        String name = scnr.next();
+        System.out.println("Hello " + name + ".");
     }
 }
 
